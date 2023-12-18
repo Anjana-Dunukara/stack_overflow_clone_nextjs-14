781de923735b501422bbee0adcfb9494fb22cb50
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-12-18 19:47:50 +0530

    setup basic routing

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignIn = () => {
+  return <div className="">Sign-In</div>;
+};
+
+export default SignIn;
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">NextJS 14 we&apos;re comming!!</h1>
-    </div>
-  );
-}
