8394bd20f03aae56f86476967bebed851524e1e3
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-12-20 16:28:59 +0530

    create GlobalSearch component uisng shadcn input

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -10,7 +10,7 @@ import {
   MenubarTrigger,
 } from "@/components/ui/menubar";
 import Image from "next/image";
-import { themes } from "@/constants/constants";
+import { themes } from "@/constants";
 
 const Theme = () => {
   const { mode, setMode } = useTheme();
