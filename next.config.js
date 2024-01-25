b37e94700d53783123bf46604ea18e7ae43b5b34
Anjana Dunukara <anjanaheshanthach@gmail.com>
2024-01-25 21:20:56 +0530

    setup for deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    mdxRs: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
   images: {
     remotePatterns: [
       {
