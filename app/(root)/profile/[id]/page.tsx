f319f02a369d8972338d3f226f530526a8587f44
Anjana Dunukara <anjanaheshanthach@gmail.com>
2024-01-25 20:58:04 +0530

    fix hydration error in profile answer tab

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -35,7 +35,7 @@ const Page = async ({ params, searchParams }: URLProps) => {
             alt="profile picture"
             width={140}
             height={140}
-            className="h-[140px] w-[140px] rounded-full object-cover"
+            className="size-[140px] rounded-full object-cover"
           />
 
           <div className="mt-3">
